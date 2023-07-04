0dc2b69c7244ce484227cdaeabcd6c84d9a670a1
Brian Murphy <gurudvlp@gmail.com>
2023-07-03 21:28:54 -0700

    Another round of debugging.

--- show-history.py
+++ show-history.py
@@ -10,7 +10,7 @@ rows = dbCursor.fetchall()
 
 data = []
 for row in rows:
-	row["time_make"] = str(row[4] // 3600)) + ":" + str((row[4] % 3600) // 60)) + ":" + str((row[4] % 3600) % 60))
+	row["time_make"] = str(row[4] // 3600) + ":" + str((row[4] % 3600) // 60) + ":" + str((row[4] % 3600) % 60)
 	
 	dt = datetime.datetime.fromtimestamp(row["started_at"])
 	prettyDate = dt.strftime("%Y-%m-%d %H:%M:%S")
